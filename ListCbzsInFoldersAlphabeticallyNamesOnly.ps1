686121c1c8717c97187ac79970cf5df18efe6d1a
ptgullas <4305689+ptgullas@users.noreply.github.com>
2020-09-03 11:12:44 -0400

    add comments

--- ListCbzsInFoldersAlphabeticallyNamesOnly.ps1
+++ ListCbzsInFoldersAlphabeticallyNamesOnly.ps1
@@ -1,8 +1,12 @@
+# get all *cbz files under current folder (& subfolders)
 $cbzs = Get-ChildItem -Name -Recurse -Include *.cbz
+# create empty array
 [System.Collections.ArrayList]$cbzNames = @()
+
 foreach ($cbz in $cbzs) {
     $cbzNameOnly = $cbz |Split-Path -Leaf
     $cbzNames += $cbzNameOnly
 }
+
 $sortedCbzNames = $cbzNames |Sort-Object
-$sortedCbzNames
+$sortedCbzNames | Out-File cbzsInTree.txt
